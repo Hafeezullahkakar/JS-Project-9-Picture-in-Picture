aac704a968438a516c574d913c9a8d40e5a05df1
hafeezullahkakar <hafeezullahkakar@gmail.com>
2021-04-08 11:43:55 +0500

    added media stream method in js

---
+++ script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+
+//Ask user to select media stream, pass to video element and then play
+
+async function selectMediaStream(){
+    try{
+        something
+    }
+    catch(error){
+        console.log('Whoops, error here:', error)
+    }
+}
+
+
+// On load
+selectMediaStream();
